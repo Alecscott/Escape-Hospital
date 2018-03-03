81045cc8706006339e2095afdad2c88d4770a492
= <sam.gilroy2@gmail.com>
2018-03-02 18:43:15 -0600

    PlayerManager + SceneManager

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+
+	private Scene pCurrentScene;
+
+	// public accessors
+	public Scene getCurrentScene(){
+		return getInstance().pCurrentScene;
+	}
+	public Scene setCurrentScene(Scene pNewScene){
+		return getInstance().pCurrentScene = pNewScene;
+	}
+
+	//private functions
+	private static SceneManager instance;
+
+	private SceneManager(){
+	}
+
+	private static SceneManager getInstance(){
+		if (instance == null) {
+			instance = new SceneManager ();
+		}
+		return instance;
+	}
+}
