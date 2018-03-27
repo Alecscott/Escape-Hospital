7c6c9abe36499112978c9453b490b852df971238
= <sam.gilroy2@gmail.com>
2018-03-27 15:55:01 -0500

    Scene Transitions Added (Scene.cs) and fixed CameraRig prefab

--- Assets/Scripts/Scene.cs
+++ Assets/Scripts/Scene.cs
@@ -1,16 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Scene : MonoBehaviour {
 
+	public string levelName;
+
 	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void onTriggerEnter(Collider col){
+		if (col.CompareTag ("Player")) {
+			SceneManager.LoadScene (levelName);
+		}
 	}
 }
