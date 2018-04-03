4812427093b0f52036b8ac4b199377d872cc83d3
= <sam.gilroy2@gmail.com>
2018-04-03 14:36:40 -0500

    fixed fade script

--- Assets/Scripts/Scene.cs
+++ Assets/Scripts/Scene.cs
@@ -2,18 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Scene : MonoBehaviour {
 
+	public Image fadeOutImg;
+	public float fadeSpeed = 0.8f; 
+
 	public string levelName;
+
+	void OnEnable(){
+		StartCoroutine (Fade(1,0));
+	}
 
 	// Use this for initialization
 	void OnTriggerEnter(Collider col){
-		Debug.Log (col.tag);
 		if (col.CompareTag ("Player")) {
 			Debug.Log ("Got to Scene Load");
-			Initiate.Fade (levelName, Color.black, 2.0f);
-			//SceneManager.LoadScene (levelName);
+			StartCoroutine (FadeAndLoadScene(0,1));
+		}
+	}
+
+	private IEnumerator Fade(float fadeStart,float fadeEnd){
+		Debug.Log ("Fading");
+		if (fadeStart > fadeEnd) {
+			Debug.Log("In");
+			while (fadeStart >= fadeEnd) {
+				SetColorImage (ref fadeStart, fadeEnd);
+				yield return null;
+			}
+			fadeOutImg.enabled = false;
+		} else {
+			Debug.Log ("Out");
+			fadeOutImg.enabled = true;
+			while (fadeStart <= fadeEnd) {
+				SetColorImage (ref fadeStart,fadeEnd);
+				yield return null;
+			}
+		}
+	}
+
+	private IEnumerator FadeAndLoadScene(float fadeStart, float fadeEnd) 
+	{
+		yield return Fade(fadeStart, fadeEnd);
+		SceneManager.LoadScene(levelName);
+	}
+
+	private void SetColorImage(ref float alpha, float end)
+	{
+		fadeOutImg.color = new Color (fadeOutImg.color.r,fadeOutImg.color.g, fadeOutImg.color.b, alpha);
+		if (alpha < end) {
+			alpha += Time.deltaTime * (1.0f / fadeSpeed);
+		} else {
+			alpha -= Time.deltaTime * (1.0f / fadeSpeed);
 		}
 	}
 }
