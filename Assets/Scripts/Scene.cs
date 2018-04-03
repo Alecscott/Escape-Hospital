8c50b5dbc659239abe2685a54df37937e1380ed8
Elser <AELSER@depaul.edu>
2018-04-03 13:24:43 -0500

    Adding more objects to populate the scene with

---
+++ Assets/Scripts/Scene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour {
+
+	public string levelName;
+
+	// Use this for initialization
+	void OnTriggerEnter(Collider col){
+		Debug.Log (col.tag);
+		if (col.CompareTag ("Player")) {
+			Debug.Log ("Got to Scene Load");
+			Initiate.Fade (levelName, Color.black, 2.0f);
+			//SceneManager.LoadScene (levelName);
+		}
+	}
+}
