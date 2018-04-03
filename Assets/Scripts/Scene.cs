9ed8ed0a97ead4ac0655318f60966d14e835117f
Lutz <QLUTZ@depaul.edu>
2018-04-03 17:53:42 -0500

    Added maze of baby carriages to maternity ward. Added baby and carriage prefabs to scene.

---
+++ Assets/Scripts/Scene.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Scene : MonoBehaviour {
+
+	public Image fadeOutImg;
+	public float fadeSpeed = 0.8f; 
+
+	public string levelName;
+
+	void OnEnable(){
+		StartCoroutine (Fade(1,0));
+	}
+
+	// Use this for initialization
+	void OnTriggerEnter(Collider col){
+		if (col.CompareTag ("Player")) {
+			Debug.Log ("Got to Scene Load");
+			StartCoroutine (FadeAndLoadScene(0,1));
+		}
+	}
+
+	private IEnumerator Fade(float fadeStart,float fadeEnd){
+		Debug.Log ("Fading");
+		if (fadeStart > fadeEnd) {
+			Debug.Log("In");
+			while (fadeStart >= fadeEnd) {
+				SetColorImage (ref fadeStart, fadeEnd);
+				yield return null;
+			}
+			fadeOutImg.enabled = false;
+		} else {
+			Debug.Log ("Out");
+			fadeOutImg.enabled = true;
+			while (fadeStart <= fadeEnd) {
+				SetColorImage (ref fadeStart,fadeEnd);
+				yield return null;
+			}
+		}
+	}
+
+	private IEnumerator FadeAndLoadScene(float fadeStart, float fadeEnd) 
+	{
+		yield return Fade(fadeStart, fadeEnd);
+		SceneManager.LoadScene(levelName);
+	}
+
+	private void SetColorImage(ref float alpha, float end)
+	{
+		fadeOutImg.color = new Color (fadeOutImg.color.r,fadeOutImg.color.g, fadeOutImg.color.b, alpha);
+		if (alpha < end) {
+			alpha += Time.deltaTime * (1.0f / fadeSpeed);
+		} else {
+			alpha -= Time.deltaTime * (1.0f / fadeSpeed);
+		}
+	}
+}
