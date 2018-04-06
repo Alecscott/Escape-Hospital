e721b2ed4385bd4291a932ca65e1cabcc888405f
Elser <AELSER@depaul.edu>
2018-04-06 16:25:04 -0500

    Updated to firing particle system and Working AI

--- Assets/Scripts/ParticleShoot.cs
+++ Assets/Scripts/ParticleShoot.cs
@@ -6,6 +6,8 @@ public class ParticleShoot : MonoBehaviour {
 	
 	public ParticleSystem particleEffect;
 	public bool firing;
+	public GameObject player;
+	private ParticleSystem p;
 
 	// Use this for initialization
 	void Start () {
@@ -14,11 +16,17 @@ public class ParticleShoot : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if((Input.GetAxis("RHandTrigger") == 1 || Input.GetAxis("LHandTrigger") == 1)){
-			particleEffect.Play ();
-		}
-		if(Input.GetAxis("RHandTrigger") == 0){
+		if((Input.GetAxis("RightMainTrigger") == 1 || Input.GetAxis("LeftMainTrigger") == 1)){
+			p = ParticleSystem.Instantiate (particleEffect);
+			Debug.Log ("firing");
 			firing = true;
+			p.transform.position = player.transform.position;
+			p.Play ();
+		}
+		if(Input.GetAxis("RightMainTrigger") == 0|| Input.GetAxis("LeftMainTrigger") == 0){
+			//Debug.Log ("stop firing");
+			ParticleSystem.Destroy (p);
+			firing = false;
 		}
 	}
 }
