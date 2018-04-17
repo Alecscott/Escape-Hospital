829784e533a63c156581254e74db55221d7c216a
Elser <AELSER@depaul.edu>
2018-04-17 12:33:31 -0500

    Editing lights in materinty ward and making doors

---
+++ Assets/Scripts/PatrolAI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrolAI : MonoBehaviour {
+
+	public Transform[] points;
+	private int destPoint = 0;
+	private NavMeshAgent agent;
+	public bool chasingPlayer = false;
+	public int timeChase;
+	private Transform player;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+		agent.autoBraking = false;
+		GotoNextPoint ();
+	}
+
+	void GotoNextPoint(){
+		if (points.Length == 0) {
+			return;
+		}
+
+		agent.destination = points [destPoint].position;
+		destPoint = (destPoint + 1) % points.Length;
+	}
+	// Update is called once per frame
+	void Update () {
+		StartCoroutine (FindPlayer());
+		if (!agent.pathPending && agent.remainingDistance < 0.5f && !chasingPlayer) {
+			GotoNextPoint ();
+		} else if (chasingPlayer) {
+			agent.destination = player.position;
+		}
+	}
+	IEnumerator FindPlayer(){
+		RaycastHit hit;
+			if (Physics.Raycast (transform.position, transform.TransformDirection (Vector3.forward), out hit)) {
+				if (hit.collider.CompareTag ("Player")) {
+					chasingPlayer = true;
+					player = hit.transform;
+					//Debug.Log ("Raycast Hit");
+				} else {
+					if (chasingPlayer == true) {
+						yield return new WaitForSeconds (timeChase);
+					}
+					chasingPlayer = false;
+				}
+			}
+		}
+
+
+}
